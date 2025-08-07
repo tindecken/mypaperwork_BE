bad1a8d16c80299518617d6996c5fca26ab3cbe4
hnu <hnu@sucafina.com>
2025-08-07 11:04:20 +0700

    refactor: remove old getByUserId controller and update routing for paperworks

--- src/index.ts
+++ src/index.ts
@@ -10,7 +10,6 @@ import { getById } from './controllers/paperworks/getById';
 import { updatePaperWork } from './controllers/paperworks/update';
 import { deletePaperWork } from './controllers/paperworks/delete';
 import { getCategories } from './controllers/categories/get';
-import { getByUserId_old } from './controllers/paperworks/getByUserId_old';
 import { getByUserId } from './controllers/paperworks/getByUserId';
 import { getByCategoryId } from './controllers/paperworks/getByCategoryId';
 import { updateCategoriesByPaperworkId } from './controllers/paperworks/updateCategories';
@@ -71,7 +70,6 @@ app.route('/paperworks', getById)
 app.route('/paperworks', updatePaperWork)
 app.route('/paperworks', deletePaperWork) //
 app.route('/paperworks', getByUserId) // get all paperworks by user id (new version, don't depend on categoy)
-app.route('/paperworks', getByUserId_old) // get all paperworks by user id (old version, depend on category)
 app.route('/paperworks', getByCategoryId) // get all paperworks by category id
 app.route('/paperworks', updateCategoriesByPaperworkId) // update categories for paperwork
 app.route('/paperworks', createShareLink) // create share link for paperwork
